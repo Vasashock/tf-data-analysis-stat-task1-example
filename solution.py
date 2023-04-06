2778f5ed0a9053ecac69e680da0b51044035fcaf
Vasashock <60927408+Vasashock@users.noreply.github.com>
2023-04-06 18:12:18 +0300

    Update task1

--- solution.py
+++ solution.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 89018174 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+    a = x.mean() / (10**2)
+    s = ((np.exp(1) + 35)**2 - 1) / 12
+    a_estimate = a - s / np.sqrt(len(x))
     return x.mean() # Ваш ответ
