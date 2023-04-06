7362c384466b283b55ef640405da09aa82e52ef5
Vasashock <60927408+Vasashock@users.noreply.github.com>
2023-04-06 18:49:59 +0300

    Update_3

--- solution.py
+++ solution.py
@@ -5,8 +5,8 @@ import numpy as np
 chat_id = 89018174 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    a = x.mean() / (10**2)
-    s = ((np.exp(1) + 35)**2 - 1) / 12
-    a_estimate = a - s / np.sqrt(len(x))
-    return a_estimate
+    loc, scale = -35, 1
+    expon_rv = expon(loc=loc, scale=scale)
+    mu = expon_rv.mean()
+    return (x.mean()-mu)/10 ** 2
  # Ваш ответ
